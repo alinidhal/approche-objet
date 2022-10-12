9633ce29275455424782968ec1c353c5a1cfccd7
Ali NIDHAL <ali.nidhal@hotmail.com>
2022-10-12 17:45:35 +0200

    Auto-formation : Excercice Cercle v2

--- src/fr/diginamic/banque/entites/TestCercle.java
+++ src/fr/diginamic/banque/entites/TestCercle.java
@@ -3,9 +3,10 @@ package fr.diginamic.banque.entites;
 public class TestCercle {
 
 	public static void main(String[] args) {
-		Cercle cercle = new Cercle(2);
+		Cercle cercle1 = new Cercle(2);
+		Cercle cercle2 = new Cercle(3);
 	
-		System.out.println(cercle.toString());
+		System.out.println(cercle1.toString()+ "\n"+ cercle2.toString());
 
 	}
 
