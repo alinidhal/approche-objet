0a5d85824c399218f1950fc3d631841b0d90de6b
Ali NIDHAL <ali.nidhal@hotmail.com>
2022-10-14 14:39:19 +0200

    Liste et ArrayList : Excercice 1

---
+++ src/listes/CreationListe.java
@@ -0,0 +1,20 @@
+package listes;
+
+import java.util.ArrayList;
+
+public class CreationListe {
+
+	public static void main(String[] args) {
+		
+
+		ArrayList<Integer> nombre = new ArrayList<Integer>();
+		for(int i=1; i<=100; i++) {
+			nombre.add(i);
+
+		}
+		
+		
+		System.out.println(nombre.size());
+	}
+
+}
